c1f6ef2ad5be1bb6f49199aac664f0ca8402cbc3
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2023-10-05 10:04:32 +0530

    Add files via upload

---
+++ forward.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float x[15],y[15],X,prod=1,s,h,d;
+	int i,j,n;
+	printf("\tOutput :\n");
+	printf("Enter the number of points : ");
+	scanf("%d",&n);
+	printf("Enter the value of X :");
+	scanf("%f",&X);
+	printf("Enter the value of x and y :\n");
+	printf("x \t y=f(x)\n");
+	for(i=1;i<=n;i++){
+		scanf("%f \t %f",&x[i],&y[i]);
+	}
+	h=x[2]-x[1];
+	printf("\n h=%f \n",h);
+	s=(X-x[1])/h;
+	d=y[1];
+	printf("The s and d is : %f\t%f",s,d);
+	for(i=1;i<n-1;i++){
+		for(j=1;j<n-1;j++){
+			y[j]=y[j+1]-y[j];
+			printf("\n%f\n",y[j]);
+		}
+		prod=prod*(s-i+1)/i;
+		d=d+prod*y[1];
+		printf("\n%f\n",d);
+	}
+	printf("The result is : ");
+	printf("y(%1.2f) = %3.5f",X,d);
+}
